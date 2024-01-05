57cf5d8a5d536c734d0c7db7ceb3911721aef925
Rustam Mamedov <rustam.mamedov230@gmail.com>
2024-01-05 15:55:50 -0500

    update main

--- main.py
+++ main.py
@@ -9,16 +9,20 @@ import typer
 from enum import Enum
 from pathlib import Path
 from typing import Optional
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
-class Algorithm(str, Enum):
+class Encoder(str, Enum):
     cjxl = "cjxl"
     cwp2 = "cwp2"
     flif = "flif"
 
 EFFORT_RANGES = {
-    Algorithm.cjxl: range(1, 10),
-    Algorithm.cwp2: range(0, 10),
-    Algorithm.flif: range(0, 101),
+    Encoder.cjxl: range(1, 10),
+    Encoder.cwp2: range(0, 10),
+    Encoder.flif: range(0, 101),
 }
 
 app = typer.Typer()
@@ -36,16 +40,22 @@ def compress_with_flif(file, output_path, effort=60):
     result = subprocess.run(["./flif", "-E", str(effort), file,  output_path + ".flif"], capture_output=True, encoding="utf-8")
     return result
 
-def run_compression(file, output_path, compression_function, format_name, **kwargs) -> (int, int, float):
-    start_time = time.time()
-    result = compression_function(file, output_path, **kwargs)
-    end_time = time.time()
+def run_compression(file, output_path, compression_function, format_name, effort_range, **kwargs) -> (int, int, float):
+    results = []
 
-    elapsed_time = end_time - start_time
-    compressed_file_size = os.path.getsize(output_path + "." + format_name)
-    original_file_size = os.path.getsize(file)
+    for effort in effort_range:
+        print(file, effort)
+        start_time = time.time()
+        result = compression_function(file, f"{output_path}_{effort}", effort=effort, **kwargs)
+        end_time = time.time()
 
-    return (original_file_size, compressed_file_size, elapsed_time)
+        elapsed_time = end_time - start_time
+        compressed_file_size = os.path.getsize(f"{output_path}_{effort}.{format_name}")
+        original_file_size = os.path.getsize(file)
+
+        results.append((os.path.basename(file), original_file_size, compressed_file_size, elapsed_time, effort))
+
+    return results 
 
 
 def write_to_csv(results, csv_filename):
@@ -66,14 +76,58 @@ def write_to_csv(results, csv_filename):
             })
 
 @app.command()
-def visualize(file):
-    pass
+def plot(file):
+    df = pd.read_csv(file)
+
+    # Plot for Compression Ratio
+    sns.set(style="whitegrid")
+    plt.figure(figsize=(12, 8))
+    ax1 = plt.axes()
+    boxplot1 = sns.boxplot(x='Effort', y='Compression Ratio', data=df, ax=ax1)
+    boxplot1.set(xlabel='Effort', ylabel='Compression Ratio', title='Compression Ratio vs. Effort')
+
+    for i, group in enumerate(df.groupby('Effort')):
+        effort, effort_data = group
+        max_index = effort_data['Compression Ratio'].idxmax()
+        filename_max = df.loc[max_index, 'Filename']
+        ax1.text(i, effort_data['Compression Ratio'].max(), f'Max: {filename_max}', color='red', weight='bold',
+                 ha='center', va='center', fontsize=8)
+        
+        min_index = effort_data['Compression Ratio'].idxmin()
+        filename_min = df.loc[min_index, 'Filename']
+        ax1.text(i, effort_data['Compression Ratio'].min(), f'Min: {filename_min}', color='red', weight='bold',
+                 ha='center', va='center', fontsize=8)
+
+    plt.show()
+
+    # Plot for Time Elapsed
+    sns.set(style="whitegrid")
+    plt.figure(figsize=(12, 8))
+    ax2 = plt.axes()
+    boxplot2 = sns.boxplot(x='Effort', y='Time Elapsed (seconds)', data=df, ax=ax2)
+    boxplot2.set(xlabel='Effort', ylabel='Time Elapsed (seconds)', title='Time Elapsed vs. Effort')
+
+    ax2.set_yscale('log')
+
+    for i, group in enumerate(df.groupby('Effort')):
+        effort, effort_data = group
+        max_index = effort_data['Time Elapsed (seconds)'].idxmax()
+        filename_max = df.loc[max_index, 'Filename']
+        ax2.text(i, effort_data['Time Elapsed (seconds)'].max(), f'Max: {filename_max}', color='red', weight='bold',
+                 ha='center', va='center', fontsize=8)
+        
+        min_index = effort_data['Time Elapsed (seconds)'].idxmin()
+        filename_min = df.loc[min_index, 'Filename']
+        ax2.text(i, effort_data['Time Elapsed (seconds)'].min(), f'Min: {filename_min}', color='red', weight='bold',
+                 ha='center', va='center', fontsize=8)
+
+    plt.show()
 
 @app.command()
-def run(algorithm: Algorithm = typer.Option(
+def run(encoder: Encoder = typer.Option(
         None,
-        "--algorithm",
-        help="Specify a single compression algorithm to use",
+        "--encoder",
+        help="Specify an encoder. Use this option if you want to compress using only one encoder.",
         show_choices=True,
     ), input_dir: Path = typer.Argument(
         os.getcwd(),
@@ -83,35 +137,32 @@ def run(algorithm: Algorithm = typer.Option(
         help="Specify path to output directory",
     )):
 
+    jxl_results = []
+    wp_results = []
+    flif_results = []
+
     files = [os.path.join(input_dir, f) for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
 
-    results = []
-
     for file in files:
+        output_path = os.path.join(output_dir, os.path.basename(file).split('.')[0])
 
-        output_path = os.path.join(output_dir, os.path.basename(file).split('.')[0]) 
+        if (encoder == Encoder.cjxl):
+            jxl_results.extend(run_compression(file, output_path, compress_with_jxl, "jxl", range(1, 10)))
+        elif (encoder == Encoder.cwp2):
+            wp_results.extend(run_compression(file, output_path, compress_with_webp, "wp", range(10)))
+        elif (encoder == Encoder.flif):
+            flif_results.extend(run_compression(file, output_path, compress_with_flif, "flif", range(101)))
+        else:   
+            jxl_results.extend(run_compression(file, output_path, compress_with_jxl, "jxl", range(1, 10)))
+            wp_results.extend(run_compression(file, output_path, compress_with_webp, "wp", range(10)))
+            flif_results.extend(run_compression(file, output_path, compress_with_flif, "flif", range(101)))
 
-        valid_effort_range = EFFORT_RANGES.get(algorithm, [])
+    if (jxl_results):
+        write_to_csv(jxl_results, 'compression_results_jxl.csv')
+    if (wp_results):
+        write_to_csv(wp_results, 'compression_results_wp.csv')
+    if (flif_results):
+        write_to_csv(flif_results, 'compression_results_flif.csv')
 
-        for effort in range(1, 10):
-            result = run_compression(file, output_path+"_"+str(effort), compress_with_jxl, "jxl", effort=effort)
-            results.append((os.path.basename(file), *result, effort))
-        
-        write_to_csv(results, 'compression_results_jxl.csv')
-        results = []
-
-        for effort in range(0,10):
-            result = run_compression(file, output_path+"_"+str(effort), compress_with_webp, "wp2", effort=effort)
-            results.append((os.path.basename(file), *result, effort))
-
-        write_to_csv(results, 'compression_results_wp.csv')
-        results = []
-        
-        for effort in range(0, 101):
-            filename = str(uuid.uuid4())
-            result = run_compression(file, output_path+"_"+str(effort), compress_with_flif, "flif", effort=effort)
-            results.append((os.path.basename(file), *result, effort))
-
-        write_to_csv(results, 'compression_results_flif.csv')
 if __name__ == '__main__':
     app()
